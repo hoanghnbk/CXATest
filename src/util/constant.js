6b10a7bc57e8ab96438c26ffd1e0791c04626d78
hoanghoang <hoanghn.bk@gmail.com>
2019-09-02 15:41:12 +0700

    add base url missing

--- src/util/constant.js
+++ src/util/constant.js
@@ -2,3 +2,4 @@ import {Dimensions} from 'react-native';
 export const POSTER_WIDTH=Dimensions.get('window').width;
 
 export const POSTER_RATIO=2/3;
+export const BASE_IMAGE_URL='http://image.tmdb.org/t/p/w';
